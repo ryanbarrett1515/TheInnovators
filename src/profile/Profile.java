0827d01a666a8f61d77fd3645a45185af5ecda92
ryanbarrett1515 <barrettra@g.cofc.edu>
2019-08-08 09:32:19 -0400

    some stuff, added .gitignore

---
+++ src/profile/Profile.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profile;
+
+import java.util.List;
+
+/**
+ *
+ * @author ryanb
+ */
+public class Profile {
+    private String firstName;
+    private String lastName;
+    private String location;
+    private String jobTitle;
+    private int age;
+    private List<String> hobbies;
+    private List<String> sports;
+    private List<String> placesLived;
+    private List<String> foods;
+    
+    public Profile() {
+        
+    }
+}
